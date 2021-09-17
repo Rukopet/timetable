53962c0ea8395c0d0c219ef9c5139ba4b2c06969
IvnAfanasiev <75682401+IvnAfanasiev@users.noreply.github.com>
2021-09-17 17:43:01 +0300

    select pair disciplines added

--- school/js/subjects.js
+++ school/js/subjects.js
@@ -11,36 +11,37 @@ discipline = ["Математика", "Русский язык", "Литерат
 //    $(event.target).next().css("display", "none");
 //    }
 //}
-
+// <option value="pair' + i + '"  - пример
 $(function() {
-    
+
     console.log("hello subjects js");
 
-    discipline.forEach((item, i, discipline) => {
-        $('#pair').append('<option value="pair1">' + discipline[i] + '</option>');
-    });
-    
+
+
     var parent = $(".discipline_container");
     for (let i = 0; i < discipline.length; i++) {
         const item = discipline[i];
         var id = "disp_one_cont" + i;
         parent.append("<div class='discipline' id='" + id + "' " +
             +"> <div class='discipline" + i + "' " + "id='discipline" + i + "' " + "> " + item + "  </div>   </div>");
-    
+
         var innerParent = $("#" + id);
-        innerParent.append("<div class='discipline-chek'> <input type='checkbox'  name='discipline" + i + "' " + " value='discipline" + i + "' " + " checked>     <input type='checkbox' class='discipline1-pair' name='discipline1-pair' value='discipline1-pair'> </div>");
+        innerParent.append("<div class='discipline-chek'> <input type='checkbox'  name='discipline" + i + "' " + " value='discipline" + i + "' " + " checked>     <input type='checkbox' class='discipline1-pair' name='discipline1-pair' value='discipline1-pair'> <select class='pair" + i + "' id='pair" + i + "' multiple name='pair" + i + "'><option disabled>укажите второй предмет</option></select></div>");
         //innerParent.append("<div class='discipline-chek'> <input type='checkbox'  name='discipline" + i + "' " + " value='discipline" + i + "' " + " checked>     <input type='checkbox' class='discipline1-pair' name='discipline1-pair' value='discipline1-pair'> <input type='checkbox' class='discipline2-pair' name='discipline2-pair' value='discipline2-pair'> </div>");
         //innerParent.find('.discipline1-pair').change(f)
         //
     }
 
+    discipline.forEach((item, i, discipline) => {
+        $("#pair0").append('<option value="pair1">' + discipline[i] + '</option>');
+    });
 
-   // $('#' + id).is(":checked")
+    // $('#' + id).is(":checked")
 
     $("#subjects_next_page").click(function() {
         console.log('Кнопка нажата!');
         var subjects = getAllCurrent();
-        localStorage.setItem("subjects", JSON.stringify(subjects));        
+        localStorage.setItem("subjects", JSON.stringify(subjects));
     })
 
 });
@@ -48,12 +49,12 @@ $(function() {
 function getAllCurrent() {
 
     var arr = [];
-    $('.discipline_container').children('div').each(function () {
+    $('.discipline_container').children('div').each(function() {
         console.log(this); // "this" is the current element in the loop
 
-        var str = $( this).text().trim();
-        arr.push(str);        
+        var str = $(this).text().trim();
+        arr.push(str);
     });
 
-    return arr;  
+    return arr;
 }
