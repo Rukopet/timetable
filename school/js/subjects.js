effe5a38fec3cdf39ef1f6138e836a6536505fa8
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2021-09-18 17:47:24 +0300

    add handlers on all select discipline

--- school/js/subjects.js
+++ school/js/subjects.js
@@ -1,17 +1,5 @@
 discipline = ["Математика", "Русский язык", "Литература", "Иностранный язык", "История", "Физическая культура", "Музыка", "Технология", "Химия", "Биология", "Физика", "Экология", "География", "Естествознание", "Астрономия", "Окружающий мир", "ИЗО", "Обществознание", "Информатика", "Геометрия"]
 
-//et state = {view: {discipline1-pair: false}}
-
-//const f = (event) => {
-//    if (event.target.checked){
-//        $(event.target).next().css("display", "inline-block");
-//    }
-//    else
-//    {
-//    $(event.target).next().css("display", "none");
-//    }
-//}
-// <option value="pair' + i + '"  - пример
 $(function() {
 
     console.log("hello subjects js");
@@ -24,30 +12,28 @@ $(function() {
             +"> <div class='discipline" + i + "' " + "id='discipline" + i + "' " + "> " + item + "  </div>   </div>");
 
         var innerParent = $("#" + id);
-                innerParent.append("<div class='discipline-chek'> <input type='checkbox'  name='discipline" + i + "' " + " value='discipline" + i + "' " + " checked>     <input type='checkbox' class='discipline-pair" + i +"' name='discipline-pair" + i +"' value='discipline-pair" + i +"'> <div class='select_pair'><select class='pair' id='pair' multiple name='pair" + i + "'><option disabled>укажите второй предмет</option></select></div></div>");
+                innerParent.append("<div class='discipline-chek'> <input type='checkbox'  name='discipline" + i + "' " + " value='discipline" + i + "' " + " checked>     <input type='checkbox' class='discipline-pair" + i +"' name='discipline-pair" + i +"' value='discipline-pair" + i +"'> <div class='select_pair'><select class='pair' id='pair" + i + "' name='pair" + i + "'><option disabled>укажите второй предмет</option></select></div></div>");
 
-        //innerParent.append("<div class='discipline-chek'> <input type='checkbox'  name='discipline" + i + "' " + " value='discipline" + i + "' " + " checked>     <input type='checkbox' class='discipline1-pair' name='discipline1-pair' value='discipline1-pair'> <input type='checkbox' class='discipline2-pair' name='discipline2-pair' value='discipline2-pair'> </div>");
-        //innerParent.find('.discipline1-pair').change(f)
-        //
+      
+        var disciplineId = ".discipline-pair" + i;
+        console.log(disciplineId)
+        $(disciplineId).on("click", function(){
+            
+            var id = i;
+            console.log("hello " + id)
+            if ($('.discipline-pair' + id).prop('checked')) {
+                $('.select_pair').fadeIn().show();
+                console.log('hide-show');            
+                discipline.forEach((item, i, discipline) => {
+                $("#pair" + id).append('<option value="pair1">' + discipline[i] + '</option>');
+            })}
+            else {
+                
+            }
+        });
     }
 
-
-    getAllCurrent();
-
-    $(".discipline-pair0").click(function(){
-        
-        if ($('.discipline-pair0').prop('checked')) {
-            $('.select_pair').fadeIn().show();
-            console.log('hide-show');
-            discipline.forEach((item, i, discipline) => {
-            $("#pair").append('<option value="pair1">' + discipline[i] + '</option>');
-        })}
-        else {
-            
-        }
-    });
-
-    // $('#' + id).is(":checked")
+    getAllCurrent();    
 
     $("#subjects_next_page").click(function() {
         console.log('Кнопка нажата!');
