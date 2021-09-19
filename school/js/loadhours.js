39a799e72f8656f517746754b6a71358a9bcfd9f
ZabiyakinM <zabiyakin.makar@gmail.comm>
2021-09-19 20:26:15 +0300

    generate json for loadhours

--- school/js/loadhours.js
+++ school/js/loadhours.js
@@ -1,43 +1,45 @@
-$(function() {
+$(function () {
 
     console.log("hello loadhour");
-
     var groups = localStorage.getItem("groups");
 
-
     var subjects = localStorage.getItem("subjects");
-     var  _subjects = JSON.parse(subjects);
-
-
-    var _groups =  JSON.parse(groups);
+    var _subjects = JSON.parse(subjects);
+    
+    var _groups = JSON.parse(groups);
     console.log(_groups);
 
     generateTable(_subjects, _groups);
 
+    $("#loadhours_next_btn").click(function () {
 
-    $("#loadhours_next_btn").click(function() { 
-            
-        
-        // todo
-        // generateJson() call
-        // и положить в локал стораж
+        var result = getJson();
 
-        window.location.href = "./frame4.html";
+        if(result.length > 0) {
+
+            var json = JSON.stringify(result);    
+
+            localStorage.setItem("loadhours", json);
+
+            window.location.href = "./frame4.html";
+        } else {
+            alert("не заполнили нагрузку")
+        }                
     })
 });
 
-// todo https://github.com/Rukopet/timetable/blob/utils/timetable_genetic_algorithm/data_for_test/load_plan.json
-// example 
-// [
-//     {
-//       "num": 1,
-//       "letter": "А",
-//       "discipline": [
-//         {
-//           "discipline": "Русский язык",
-//           "load": 4
-//         },
-function generateJson(){
+function getSubjectByCell(row) {
+    var c = $("#tableSubjects").find('tr:eq(' + (row - 1) + ')').find('td:eq(' + 0 + ')');
+    return c.text().trim();
+}
+
+function getGroupByCell(column) {
+    var c = $("#tableSubjects").find('tr:eq(' + 0 + ')').find('td:eq(' + column + ')');
+    return c.text().trim();
+}
+
+// example https://github.com/Rukopet/timetable/blob/utils/timetable_genetic_algorithm/data_for_test/load_plan.json
+function getJson() {
     var result = [];
 
     var obj = {}
@@ -48,44 +50,43 @@ function generateJson(){
 
         $('td', this).each(function () {
             var value = $(this).find(":input").val();
+
+            if (value) {
             
-            if(value) {
+                var col = $(this).parent().children().index($(this));
+                var row = $(this).parent().parent().children().index($(this).parent());            
 
-                console.log($(this));
-
-                var cellIndex = $(this)[0].cellIndex;
-
-                // todo по свойствам td можно
-                // определить класс и дисциплаину по шапке таблицы и по первой колонке
-                console.log(cellIndex);
-
+                var subject = getSubjectByCell(row);                
+                var group = getGroupByCell(col).trim();                
+                
                 result.push({
-                    num: 1,
-                    letter: "",
+                    num: group.toString().charAt(0),
+                    letter: group.toString().charAt(1),
                     discipline: [
-                                {
-                                  "discipline": "Русский язык",
-                                  "load": value
-                                }]
-                            });
+                        {
+                            "discipline": subject,
+                            "load": value
+                        }]
+                });                
+            }
+        })
 
-                            console.log(value);
-            }                        
-         })
-    
     })
+
+    console.log(result);
+    return result;
 }
 
 // google - add table row foreach jquery
-function generateTable(subjects, groups){
+function generateTable(subjects, groups) {
 
     addFirstRow(groups);
     var inputRow = getInputRow(groups);
 
-    for (var i = 0; i < subjects.length; i++ ) {
+    for (var i = 0; i < subjects.length; i++) {
 
         var subject = subjects[i];
-        var tr_var ="<tr><td> " + subject + "</td>" + inputRow + "</tr>";
+        var tr_var = "<tr><td> " + subject + "</td>" + inputRow + "</tr>";
 
         $(tr_var).appendTo('#tableSubjects');
     }
@@ -101,12 +102,12 @@ function addFirstRow(groups) {
 
     var p = "Предметы"
     console.log(head);
-    var tr_var ="<tr><td> " + p + "</td>" + head + "</tr>";
+    var tr_var = "<tr><td> " + p + "</td>" + head + "</tr>";
 
     $(tr_var).appendTo('#tableSubjects');
 }
 
-function getInputRow(groups){
+function getInputRow(groups) {
     var row = "";
     for (let i = 0; i < groups.length; i++) {
         row += "<td> <input type='number'></td>"
